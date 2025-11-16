c4572bae1d20300f69b1a7175d518f5b2ddbf12c
Tim Lauridsen <tla@rasmil.dk>
2025-11-16 08:26:41 +0100

    remove some logging

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 use dnf5daemon::package::get_packages;
 use dnf5daemon::{daemon::DnfDaemon, package::DnfPackage};
 // use env_logger;
-use log::{debug, info};
+use log::debug;
 use std::error::Error;
 
 use clap::{Parser, ValueEnum};
@@ -80,12 +80,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     setup_logger(&args);
     debug!("{:?}", args);
     if args.patterns.len() > 0 {
-        info!("Starting");
         let dnf_daemon = DnfDaemon::new().await;
         dnf_daemon.base.read_all_repos().await.ok();
         let packages = get_packages(&dnf_daemon, args.patterns, &args.scope.to_string()).await;
         print_packages(&packages, args.scope);
-        info!("Ending");
     }
     Ok(())
 }
